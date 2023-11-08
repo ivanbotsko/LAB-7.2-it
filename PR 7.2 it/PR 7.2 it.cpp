65cc0d71caf327e585bdaeb6abcf3be5b4421d00
ivanbotsko <ivan.botsko.ik.2023@lpnu.ua>
2023-11-08 22:09:03 +0200

    first master commit

---
+++ PR 7.2 it/PR 7.2 it.cpp
@@ -0,0 +1,109 @@
+﻿// Lab_7_2_1.cpp
+// Боцко Іван Віталійович
+// Лабораторна робота № 7.2.1)
+// Опрацювання багатовимірних масивів ітераційними способами.
+// Варіант 4
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+using namespace std;
+
+void Create(int** a, const int n, const int Low, const int High);
+void Print(int** a, const int n);
+bool SearchMinEven(int** a, const int n, int& minEven);
+
+int SumMaxEvenOnEvenRows(int** a, const int n);
+
+int main()
+{
+    srand((unsigned)time(NULL));
+    int Low = 7;
+    int High = 65;
+    int n;
+    cout << "n = "; cin >> n;
+    int** a = new int* [n];
+    for (int i = 0; i < n; i++)
+        a[i] = new int[n];
+
+    Create(a, n, Low, High);
+    Print(a, n);
+
+    int minEven;
+    if (SearchMinEven(a, n, minEven))
+        cout << "min even = " << minEven << endl;
+    else
+        cout << "there are no even elements" << endl;
+
+    int sumMaxEvenOnEvenRows = SumMaxEvenOnEvenRows(a, n);
+    cout << "Sum of max even elements on even rows: " << sumMaxEvenOnEvenRows << endl;
+
+    for (int i = 0; i < n; i++)
+        delete[] a[i];
+    delete[] a;
+
+    return 0;
+}
+
+void Create(int** a, const int n, const int Low, const int High)
+{
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = Low + rand() % (High - Low + 1);
+}
+
+void Print(int** a, const int n)
+{
+    cout << endl;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            cout << setw(4) << a[i][j];
+        cout << endl;
+    }
+    cout << endl;
+}
+
+bool SearchMinEven(int** a, const int n, int& minEven)
+{
+    int j, k = -1;
+    for (int i = 0; i < n; i++)
+    {
+        j = n - 1 - i;
+        if (a[i][j] % 2 == 0)
+        {
+            k = i;
+            minEven = a[i][j];
+            break;
+        }
+    }
+
+    if (k == -1)
+        return false;
+    for (int i = k + 1; i < n; i++)
+    {
+        j = n - 1 - i;
+        if (a[i][j] % 2 == 0 && a[i][j] < minEven)
+        {
+            minEven = a[i][j];
+        }
+    }
+    return true;
+}
+
+int SumMaxEvenOnEvenRows(int** a, const int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i += 2)
+    {
+        int maxEven = INT_MIN;
+        for (int j = 0; j < n; j++)
+        {
+            if (a[i][j] % 2 == 0 && a[i][j] > maxEven)
+            {
+                maxEven = a[i][j];
+            }
+        }
+        sum += maxEven;
+    }
+    return sum;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,38 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR 7.2 it/PR 7.2 it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace YourMatrixFunctionsTests
+{
+    TEST_CLASS(CreateFunctionTests)
+    {
+    public:
+
+        TEST_METHOD(TestCreateFunction)
+        {
+            const int n = 5;  
+            const int Low = 9;
+            const int High = 61;
+
+            int** matrix = new int* [n];
+            for (int i = 0; i < n; i++)
+                matrix[i] = new int[n];
+
+            Create(matrix, n, Low, High);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Assert::IsTrue(matrix[i][j] >= Low && matrix[i][j] <= High);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+                delete[] matrix[i];
+            delete[] matrix;
+        }
+    };
+}
